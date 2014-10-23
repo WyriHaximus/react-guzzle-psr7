bcda7a2799598c5c6e337f36d244f8662b18d897
Cees-Jan Kiewiet <ceesjank@gmail.com>
2014-10-23 23:37:08 +0200

    Ported the getters/settings for http-client and dns over

--- src/HttpClientAdapter.php
+++ src/HttpClientAdapter.php
@@ -4,25 +4,97 @@ namespace WyriHaximus\React\RingPHP;
 
 use GuzzleHttp\Ring\Core;
 use GuzzleHttp\Ring\Future\FutureArray;
-use WyriHaximus\React\RingPHP\HttpClient\Request;
+use React\Dns\Resolver\Factory as DnsFactory;
+use React\Dns\Resolver\Resolver as DnsResolver;
+use React\EventLoop\LoopInterface;
+use React\HttpClient\Client as HttpClient;
+use React\HttpClient\Factory as HttpClientFactory;
+use WyriHaximus\React\RingPHP\HttpClient\RequestFactory;
 
 class HttpClientAdapter
 {
-    public function __construct($loop)
+    /**
+     * @param LoopInterface $loop
+     * @param HttpClient $httpClient
+     * @param DnsResolver $dnsResolver
+     */
+    public function __construct(LoopInterface $loop, HttpClient $httpClient = null, DnsResolver $dnsResolver = null, RequestFactory $requestFactory = null)
     {
         $this->loop = $loop;
 
-        $dnsResolverFactory = new \React\Dns\Resolver\Factory();
-        $dnsResolver = $dnsResolverFactory->createCached('8.8.8.8', $this->loop);
+        $this->setDnsResolver($dnsResolver);
+        $this->setHttpClient($httpClient);
+        $this->setRequestFactory($requestFactory);
+    }
 
-        $factory = new \React\HttpClient\Factory();
+    /**
+     * @param HttpClient $httpClient
+     */
+    public function setHttpClient(HttpClient $httpClient = null)
+    {
+        if (!($httpClient instanceof HttpClient)) {
+            $this->setDnsResolver($this->dnsResolver);
 
-        $this->client = $factory->create($this->loop, $dnsResolver);
+            $factory = new HttpClientFactory();
+            $httpClient = $factory->create($this->loop, $this->dnsResolver);
+        }
+
+        $this->httpClient = $httpClient;
+    }
+
+    /**
+     * @return HttpClient
+     */
+    public function getHttpClient()
+    {
+        return $this->httpClient;
+    }
+
+    /**
+     * @param DnsResolver $dnsResolver
+     */
+    public function setDnsResolver(DnsResolver $dnsResolver = null)
+    {
+        if (!($dnsResolver instanceof DnsResolver)) {
+            $dnsResolverFactory = new DnsFactory();
+            $dnsResolver = $dnsResolverFactory->createCached('8.8.8.8', $this->loop);
+        }
+
+        $this->dnsResolver = $dnsResolver;
+    }
+
+    /**
+     * @return DnsResolver
+     */
+    public function getDnsResolver()
+    {
+        return $this->dnsResolver;
+    }
+
+    /**
+     * @param RequestFactory $requestFactory
+     */
+    public function setRequestFactory(RequestFactory $requestFactory = null)
+    {
+        if (!($requestFactory instanceof RequestFactory)) {
+            $requestFactory = new RequestFactory();
+        }
+
+        $this->requestFactory = $requestFactory;
+    }
+
+    /**
+     * @return RequestFactory
+     */
+    public function getRequestFactory()
+    {
+        return $this->requestFactory;
     }
 
     public function __invoke(array $request)
     {
-        $httpRequest = new Request($request, $this->client, $this->loop);
+        $httpRequest = $this->requestFactory->create
+        ($request, $this->client, $this->loop);
         return new FutureArray($httpRequest->send());
     }
 }
