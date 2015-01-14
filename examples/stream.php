ca7adb73ef8fdc45428eeb18ad63ba3036362fbc
Cees-Jan <ceesjank@gmail.com>
2015-01-14 18:39:46 +0100

    Streaming example for #2

---
+++ examples/stream.php
@@ -0,0 +1,27 @@
+<?php
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Message\Response;
+use React\EventLoop\Factory;
+use WyriHaximus\React\RingPHP\HttpClientAdapter;
+
+// Create eventloop
+$loop = Factory::create();
+
+(new Client([
+    'handler' => new HttpClientAdapter($loop),
+]))->get('http://blog.wyrihaximus.net/', [
+    'future' => true,
+    'stream' => true,
+])->then(function(Response $response) { // Success callback
+    $body = $response->getBody();
+    while (!$body->eof()) { // Reading from the body untill there is nothing more to read
+        echo $body->read(1024);
+    }
+}, function($error) { // Error callback
+    echo $error->getMessage(), PHP_EOL;
+});
+
+$loop->run();
