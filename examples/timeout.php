0aabfa2cdfd2a721885518b9a8e866329a28a9c7
Cees-Jan <ceesjank@gmail.com>
2015-01-14 19:10:49 +0100

    Shorter timeout example

--- examples/timeout.php
+++ examples/timeout.php
@@ -15,12 +15,13 @@ $guzzle = new Client([
 ]);
 
 $guzzle->get('http://www.amazon.com/', [
-    'timeout' => 0.1,
+    'timeout' => 0.001,
     'future' => true,
 ])->then(function(Response $response) {
     echo 'Amazon completed' . PHP_EOL;
 }, function($error) {
     echo 'Amazon error' . PHP_EOL;
+    echo $error->getMessage() . PHP_EOL;
 });
 
 
